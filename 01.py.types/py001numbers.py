5c83b4347dbd478afa9fb0ba1e96b066eec7f166
johansen masterson <medalhaemerson@gmail.com>
2023-02-08 15:03:13 -0300

    exercises-py

---
+++ 01.py.types/py001numbers.py
@@ -0,0 +1,33 @@
+print(type(1), 1)
+print(type(-10), -10)
+print(type(0), 0)
+print(type(0.0), 0.0)
+print(type(2.2), 2.2)
+print(type(4E2), 4E2)
+
+# arithmetic
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 ** 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+
+# basic functions
+print(pow(5, 2))
+print(abs(-50))
+print(round(5.46))
+print(round(5.468, 2))
+print(bin(512))
+print(hex(512))
+
+# age = input('how old are you?')
+age = 32
+age = int(age)
+# pi = input('what is the value of PI?')
+pi = 3.1416
+pi = float(pi)
+
+print(age, pi)
